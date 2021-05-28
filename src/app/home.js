a11f17ec99fe94e995db341c78147c635fd5be13
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2021-05-29 05:24:47 +0900

    updated independencies

--- src/app/home.js
+++ src/app/home.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export default function Home(){
-    return (
-    <div>
-        <Container>
-            <Row>
-                <Col></Col>
-            </Row>
-        </Container>
-    </div>)
+export default function Home() {
+  return <></>;
 }
